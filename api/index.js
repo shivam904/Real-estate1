9c89d5a012c05b5a436a4a29e162d93a3ae04c7b
shivam <negishivam904@gmail.com>
2024-02-21 21:59:11 +0530

    databse connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
-
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+const db= new pg.Client({
+    user: "postgres",
+  host:"localhost",
+  database: "Real-estate",
+  password: "Kingshiva@1234",
+  port: 5432,
+});
+db.connect();
 const app= express();
 
 const port= 3000;
